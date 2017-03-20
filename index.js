92c840a9f8be9171e1311d932b0ebc67fd2626f8
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2017-03-19 22:18:04 -0300

    progressive web app

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ const rootPath = path.normalize(__dirname);
 app.use(express.static(`${rootPath}/src`));
 
 const port = 8080;
+const adress = '0.0.0.0';
 
-app.listen(port, (err) => console.log('Client listening on 8080'));
+app.listen(port, adress, (err) => console.log('Client listening on 8080'));
---
+++ src/sw.js
@@ -0,0 +1,56 @@
+var cacheName = 'worktime1-0-0';
+var filesToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'style.css',
+  'app.js',
+  '/assets/moment.min.js',
+  '/assets/vue.min.js',
+  '/assets/favicon.ico',
+  '/assets/notifications-on.svg',
+  '/assets/notifications-off.svg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      })
+      .then(function () {
+        console.log('[serviceWorker]: Intalled And Skip Waiting on Install');
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+      .then(function () {
+      console.log('[serviceWorker]: Clients Claims');
+      return self.clients.claim();
+    })
+  ); 
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
